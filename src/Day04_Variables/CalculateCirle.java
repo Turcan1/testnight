6a7dc68c908133c17dcc0f8dd3444e1f1016420e
Turcan1 <74077319+Turcan1@users.noreply.github.com>
2020-11-10 11:38:24 -0800

    test

---
+++ src/Day04_Variables/CalculateCirle.java
@@ -0,0 +1,28 @@
+package Day04_Variables;
+
+public class CalculateCirle {
+
+    public static void main(String[] args) {
+        //r:7.5
+        double r=7.5;
+        double pi =3.14;
+
+          double area=  r*r*pi;
+          double perimeter=2*r*pi;
+
+        System.out.println("5+2+"+3+4 );
+        System.out.println("5+2+"+(3+4));
+
+        System.out.println("Perimeter of the cirle is :  ");
+        System.out.println(perimeter);
+
+    }
+
+
+}
+/*
+
+Hits:Area of cirle=r*r*PI
+ Perimetre of cirle =2*R*PI
+
+ */
